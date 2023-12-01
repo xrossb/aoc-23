78b3da8d52e25c905aaeebc7a3550da643b51b0f
Edward Allen <hi@eallen.me>
2023-12-01 20:16:53 +1100

    Complete day 1 part 1

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,3 +1,28 @@
+
 fn main() {
-    println!("Hello, world!");
+    let mut sum = 0;
+    for l in std::io::stdin().lines() {
+        sum += calibration_value(l.expect("could not get stdin line"));
+    }
+    println!("{}", sum);
+}
+
+fn calibration_value(line: String) -> u64 {
+    let mut first = None;
+    let mut last = None;
+
+    for c in line.chars() {
+        if let Some(digit) = c.to_digit(10) {
+            if first.is_none() {
+                first = Some(digit);
+            }
+
+            last = Some(digit);
+        }
+    }
+
+    match (first, last) {
+        (Some(f), Some(l)) => f as u64 * 10 + l as u64,
+        _ => 0,
+    }
 }
